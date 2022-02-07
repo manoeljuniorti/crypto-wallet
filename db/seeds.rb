ac38889c5baa76d8d40eadf1525a58aa48ad731d
Manoel Júnior <manoeljuniordev@gmail.com>
2022-02-07 11:06:45 -0300

    Projeto de estudo, crypto moedas - Primeira parte finalizada.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+spinner = TTY::Spinner.new("[:spinner] Cadastrando moedas...")
+spinner.auto_spin
+
+coins = [
+          {
+            description: "Bitcoin",
+            acronym: "BTC",
+            url_image: "https://toppng.com/uploads/preview/bitcoin-png-bitcoin-logo-transparent-background-11562933997uxok6gcqjp.png"
+          },
+
+          {
+            description: "Ethereum",
+            acronym: "ETH",
+            url_image: "https://w7.pngwing.com/pngs/368/176/png-transparent-ethereum-cryptocurrency-blockchain-bitcoin-logo-bitcoin-angle-triangle-logo.png"
+          },
+
+          {
+            description: "Dash",
+            acronym: "DASH",
+            url_image: "https://flyclipart.com/thumb2/dash-coin-690652.png"
+          }
+        ]
+
+coins.each do |coin|
+  Coin.find_or_create_by!(coin)
+end
+
+spinner.success("(Cadastro concluído!)")
